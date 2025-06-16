a061682fa3f949288bf2076f0e8e32d9dac82d8d
Arman <arm2arm@gmail.com>
2025-06-16 18:11:02 +0200

    Update mymc.sh

--- mymc.sh
+++ mymc.sh
@@ -4,37 +4,7 @@
 #   ./mymc.sh local_file_or_dir s3:bucket/path
 #   ./mymc.sh s3:bucket/path local_file_or_dir
 
-set -e
 
-# Function to check which MC_ variables are missing
-missing_vars=()
-check_mc_env_vars() {
-    [ -z "$MC_ALIAS" ] && missing_vars+=("MC_ALIAS")
-    [ -z "$MC_HOST" ] && missing_vars+=("MC_HOST")
-    [ -z "$MC_ACCESS_KEY" ] && missing_vars+=("MC_ACCESS_KEY")
-    [ -z "$MC_SECRET_KEY" ] && missing_vars+=("MC_SECRET_KEY")
-}
-
-check_mc_env_vars
-
-# Try loading from .env if any are missing
-if [ ${#missing_vars[@]} -gt 0 ]; then
-    if [ ! -f ".env" ]; then
-        echo "Error: The following environment variables are not set: ${missing_vars[*]}"
-        echo "Also, .env file is not found. Please export them or provide a .env file."
-        exit 1
-    fi
-    # shellcheck disable=SC1091
-    source .env
-
-    # Clear and re-check after sourcing
-    missing_vars=()
-    check_mc_env_vars
-    if [ ${#missing_vars[@]} -gt 0 ]; then
-        echo "Error: After sourcing .env, still missing: ${missing_vars[*]}"
-        exit 1
-    fi
-fi
 
 SRC="$1"
 DST="$2"
